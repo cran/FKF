97035d224c92d0af16e1fbda8a707da3e8aab8fb
Paul Smith <paul@waternumbers.co.uk>
2024-01-30 18:00:02 +0000

    version 0.2.5

--- R/FKF-package.R
+++ R/FKF-package.R
@@ -1,12 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' @useDynLib FKF
 #' @import graphics
-# #' @import RUnit
 #' @importFrom grDevices rainbow
 #' @importFrom stats acf ccf ppoints qchisq qnorm qqnorm qqplot
-#' @exportClass fkf
 NULL
-##export("fkf", "plot.fkf")
-##exportClasses("fkf")
-## export plot method for class fkf
-##S3method("plot", "fkf")
 
